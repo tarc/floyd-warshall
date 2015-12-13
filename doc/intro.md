043056e5da7f69226976013fc1fcc927ebe22ee4
Tarcisio Rodrigues <tarcisio.genaro@gmail.com>
2015-12-13 17:07:30 -0200

    lein new

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to floyd-warshall
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/floyd_warshall/core_test.clj
@@ -0,0 +1,7 @@
+(ns floyd-warshall.core-test
+  (:require [clojure.test :refer :all]
+            [floyd-warshall.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
