a910add560c609b2597482c1a59dcc966bbad256
Tarcisio Rodrigues <tarcisio.genaro@gmail.com>
2015-12-14 02:16:13 -0200

    add setup graph code

--- src/floyd_warshall/core.clj
+++ src/floyd_warshall/core.clj
@@ -1,6 +1,61 @@
 (ns floyd-warshall.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn f-pack
+  [& ps]
+  
+  (into [] ps))
+
+
+
+(defn f-prod
+  [& fs]
+
+  (fn [col el]
+    (->>
+     (map #((% 1) (col (% 0)) el) (map-indexed vector fs))
+     (apply f-pack))))
+
+
+
+(defn update-distance
+
+  [graph v-map i j distance]
+
+  (->> (-> (graph (v-map i))     ; updates from the i-th line of graph
+           (assoc (v-map j) distance))  ; its j-th element with
+                                        ; distance
+
+       (assoc graph (v-map i)))) ; and then updates graph with this
+                                 ; i-th line
+
+
+
+(defn update-distance-undirected
+
+  [graph v-map i j distance]
+
+  (-> graph
+      (update-distance v-map i j distance)
+      (update-distance v-map j i distance)))
+
+
+
+(defn set-up
+  
+  [edges]
+
+  (let [v-set (reduce into #{} edges)
+        v-vector (into [] v-set)
+        v-map (into {} (map-indexed #(vector %2 %1)
+                                    v-vector))
+        g-matrix (into [] (repeat
+                           (count v-vector)
+                           (into [] (repeat
+                                     (count v-vector) nil))) )
+
+        g-matrix (reduce #(update-distance-undirected %1 v-map (%2 0) (%2 1) 1) 
+                         g-matrix edges)]
+
+    {:vertices v-vector
+     :map v-map
+     :graph g-matrix}))
--- test/floyd_warshall/core_test.clj
+++ test/floyd_warshall/core_test.clj
@@ -2,6 +2,32 @@
   (:require [clojure.test :refer :all]
             [floyd-warshall.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(deftest zero-packing
+  (testing "Packing no elements."
+    (is (empty? (f-pack)))))
+
+(deftest basic-packing
+  (testing "Packing two elements."
+    (is (= ((f-pack 0 1) 0)
+           0))
+    (is (= ((f-pack 0 1) 1)
+           1))))
+
+(deftest basic-function-product
+  (testing "Basic product of two functions."
+    (let [p (f-prod into conj)]
+      (is (=
+           (p (f-pack #{} []) [:0 :1])
+           (f-pack #{:0 :1} [[:0 :1]]))))))
+
+(deftest something
+  (testing ""
+    (is (=
+         (set-up [[:0 :1]
+                  [:1 :3]
+                  [:1 :2]
+                  [:2 :4]
+                  [:2 :5]])
+         {:vector [:0 :4 :1 :2 :5 :3]
+          :map {:0 0 :1 1 :2 3 :3 2} }))))
